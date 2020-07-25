48cc0beea24fce328ddc0317333e8791804f9ede
nguyenvantung9296 <tungnguyenvan9296@gmail.com>
2020-07-25 08:41:55 +0700

    Updates

---
+++ precache-manifest.c6d771f95efd7d592f2c5e8334d1e4bc.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037f05239f2c9a2b8b0529dd81646c2c",
+    "url": "/homegym/index.html"
+  },
+  {
+    "revision": "bef4829c45e75c626702",
+    "url": "/homegym/static/css/main.09720341.chunk.css"
+  },
+  {
+    "revision": "b034b45e9b40b37303f0",
+    "url": "/homegym/static/js/2.03078829.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homegym/static/js/2.03078829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef4829c45e75c626702",
+    "url": "/homegym/static/js/main.c72b98c7.chunk.js"
+  },
+  {
+    "revision": "5975107be223ae9c4002",
+    "url": "/homegym/static/js/runtime-main.58728070.js"
+  },
+  {
+    "revision": "3a75887d2a74f4ca9b476a95d8f821d3",
+    "url": "/homegym/static/media/1.3a75887d.jpg"
+  },
+  {
+    "revision": "edd48b3ffe81bde63383fff44c89537d",
+    "url": "/homegym/static/media/3-1.edd48b3f.jpg"
+  },
+  {
+    "revision": "521143993bd5d8757eec3b7b70bc6b8c",
+    "url": "/homegym/static/media/PicsArt.52114399.jpg"
+  },
+  {
+    "revision": "a08af9349cde6ba41a05a86e576ec588",
+    "url": "/homegym/static/media/arow1.a08af934.png"
+  },
+  {
+    "revision": "62cd3a23fe2fff949e785eaf5b26f9df",
+    "url": "/homegym/static/media/bg-2.62cd3a23.jpg"
+  },
+  {
+    "revision": "1a211e7ce67b6b75ba949c59ceec5102",
+    "url": "/homegym/static/media/bg-crossfit.1a211e7c.svg"
+  },
+  {
+    "revision": "3a79441ee4ff2409e79d02b40802d901",
+    "url": "/homegym/static/media/bg-price-2.3a79441e.svg"
+  },
+  {
+    "revision": "93f31482c904cd048d9f3fc44276445e",
+    "url": "/homegym/static/media/bg-testimonials.93f31482.svg"
+  },
+  {
+    "revision": "45ca3465c88e7832fc6c58cfa3dbfb99",
+    "url": "/homegym/static/media/bg2-2.45ca3465.jpg"
+  },
+  {
+    "revision": "ca0c553c27119c4473e80f71466d1b6e",
+    "url": "/homegym/static/media/choose-us.ca0c553c.jpg"
+  },
+  {
+    "revision": "757b57d802898f732c87d21c23e59f3e",
+    "url": "/homegym/static/media/ed6c933692a569fb30b4.757b57d8.jpg"
+  },
+  {
+    "revision": "0ef3a7f4dfe3bb822bb02a0894b7a06f",
+    "url": "/homegym/static/media/icon1.0ef3a7f4.svg"
+  },
+  {
+    "revision": "7713a79a53827770bfeb8bcaefbe1245",
+    "url": "/homegym/static/media/icon2.7713a79a.svg"
+  },
+  {
+    "revision": "6289427c107f4c538cbd2851579a8536",
+    "url": "/homegym/static/media/icon3.6289427c.svg"
+  },
+  {
+    "revision": "fe1b7246a4e3370445e9ca7e2430411f",
+    "url": "/homegym/static/media/icon4.fe1b7246.svg"
+  },
+  {
+    "revision": "0ad029d0852821189912e5e4463458fc",
+    "url": "/homegym/static/media/icon5.0ad029d0.svg"
+  },
+  {
+    "revision": "22b8f7045e55990690a3604b48a5b2df",
+    "url": "/homegym/static/media/image-sec3.22b8f704.jpg"
+  },
+  {
+    "revision": "dec8c169677bc54d14e0ae458389e372",
+    "url": "/homegym/static/media/image-yoga.dec8c169.jpg"
+  },
+  {
+    "revision": "10a2e56a3c460b29d780ef20e924a040",
+    "url": "/homegym/static/media/image-yoga2.10a2e56a.jpg"
+  },
+  {
+    "revision": "35c02e6599fca376bf01f043f7128f26",
+    "url": "/homegym/static/media/img-sec2.35c02e65.jpg"
+  },
+  {
+    "revision": "b1f3fb7606c3c7ddf324e1a4df888ea5",
+    "url": "/homegym/static/media/j6a-pc-banner.b1f3fb76.jpg"
+  },
+  {
+    "revision": "e5b163e7480554661b7d600216475296",
+    "url": "/homegym/static/media/modal-video.e5b163e7.jpg"
+  },
+  {
+    "revision": "7809f1ff16810c135fbff4c9875492a1",
+    "url": "/homegym/static/media/page-1_slide01.7809f1ff.jpg"
+  },
+  {
+    "revision": "3d66e04cbcda58896afde6e5fe9dcdc8",
+    "url": "/homegym/static/media/phongtap_bg1.3d66e04c.png"
+  },
+  {
+    "revision": "87cdaf36a5cf7e3ee3ab56db882aa0d0",
+    "url": "/homegym/static/media/phongtap_bg2.87cdaf36.jpg"
+  },
+  {
+    "revision": "4323a071a028e4f5e95fe971c13b30f3",
+    "url": "/homegym/static/media/pocketaudio_summary_bg02.4323a071.jpg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/homegym/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "4f1999e5698f528b1879349818c8e32a",
+    "url": "/homegym/static/media/slide-1.4f1999e5.jpg"
+  },
+  {
+    "revision": "2a4b1c2115ea1a6d4bf03fbde4ad07d7",
+    "url": "/homegym/static/media/tool_banner.2a4b1c21.png"
+  },
+  {
+    "revision": "c56837c18784c2c0faa68aa1402953c6",
+    "url": "/homegym/static/media/wheyhero1.c56837c1.jpg"
+  },
+  {
+    "revision": "cfcdb617e6c714689a7a7e0cdb175a43",
+    "url": "/homegym/static/media/wheyhero4.cfcdb617.jpg"
+  },
+  {
+    "revision": "499618fb51d15f7b2d73e734f940a536",
+    "url": "/homegym/static/media/yogo-login.499618fb.png"
+  }
+]);
